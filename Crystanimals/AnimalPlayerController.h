3abe4c4bf6c92bdb2ac4d91b8bd572f086cfd565
Leevi Matias Mäki-Kerttula <leevi221001@gmail.com>
2024-04-03 23:21:39 +0300

    Create classes Character, PlayerController, HUD

---
+++ Crystanimals/AnimalPlayerController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AnimalPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRYSTANIMALS_API AAnimalPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+
+protected:
+
+private:
+	
+};
