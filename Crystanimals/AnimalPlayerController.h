459d3d2486995ebe99c7b2132a16825a8e826404
Leevi Matias Mäki-Kerttula <leevi221001@gmail.com>
2024-04-04 22:59:46 +0300

    Add pointers to PlayerController

--- Crystanimals/AnimalPlayerController.h
+++ Crystanimals/AnimalPlayerController.h
@@ -15,9 +15,27 @@ class CRYSTANIMALS_API AAnimalPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
+	// The Input Action to map to movement.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Input|Character Movement")
+	UInputAction* ActionMove = nullptr;
+
+	// The Input Action to map to looking around.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Input|Character Movement")
+	UInputAction* ActionLook = nullptr;
+
+	// The Input Action to map to jumping.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Input|Character Movement")
+	UInputAction* ActionJump = nullptr;
 
 protected:
 
 private:
+	// Used to store a reference to the InputComponent cast to an EnhancedInputComponent.
+	UPROPERTY()
+	UEnhancedInputComponent* EnhancedInputComponent = nullptr;
+
+	// Used to store a reference to the controlled pawn.
+	UPROPERTY()
+	AAnimalCharacter* PlayerCharacter = nullptr;
 	
 };
