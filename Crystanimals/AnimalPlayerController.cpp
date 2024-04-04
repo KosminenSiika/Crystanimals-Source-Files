63b4c99b22e33bdde9788b6667782d4dcd38be35
Leevi Matias Mäki-Kerttula <leevi221001@gmail.com>
2024-04-05 00:59:47 +0300

    Implement character move, look, jump

--- Crystanimals/AnimalPlayerController.cpp
+++ Crystanimals/AnimalPlayerController.cpp
@@ -3,3 +3,88 @@
 
 #include "AnimalPlayerController.h"
 
+#include "AnimalCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+void AAnimalPlayerController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+
+	// Store a reference to the Player's Pawn.
+	PlayerCharacter = Cast<AAnimalCharacter>(aPawn);
+	checkf(PlayerCharacter, TEXT("AAnimalController derived classes should only possess AAnimalCharacter derived pawns"));
+
+	// Store a reference to the EnhancedInputComponent.
+	EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	checkf(EnhancedInputComponent, TEXT("Unable to get reference to the EnhancedInputComponent."));
+
+	// Get the local player subsystem.
+	TObjectPtr<UEnhancedInputLocalPlayerSubsystem> InputSubsystem =
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	checkf(InputSubsystem, TEXT("Unable to get reference to the EnhancedInputLocalPlayerSubsystem."));
+
+	// Wipe existing mappings, and add our mapping.
+	checkf(InputMappingContext, TEXT("InputMappingContext was not specified"));
+	InputSubsystem->ClearAllMappings();
+	InputSubsystem->AddMappingContext(InputMappingContext, 0);
+
+	// Bind the input actions.
+	// Only attempt to bind if a valid value was provided.
+	if (ActionMove)
+	{
+		EnhancedInputComponent->BindAction(ActionMove, ETriggerEvent::Triggered, this, &AAnimalPlayerController::HandleMove);
+	}
+
+	if (ActionLook)
+	{
+		EnhancedInputComponent->BindAction(ActionLook, ETriggerEvent::Triggered, this, &AAnimalPlayerController::HandleLook);
+	}
+
+	if (ActionJump)
+	{
+		EnhancedInputComponent->BindAction(ActionJump, ETriggerEvent::Triggered, this, &AAnimalPlayerController::HandleJump);
+	}
+}
+
+void AAnimalPlayerController::OnUnPossess()
+{
+	// Unbind things here...
+	EnhancedInputComponent->ClearActionBindings();
+
+	Super::OnUnPossess();
+}
+
+void AAnimalPlayerController::HandleLook(const FInputActionValue& InputActionValue)
+{
+	// Input is a Vector2D
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+
+	// Add yaw and pitch input to controller
+	AddYawInput(LookAxisVector.X);
+	AddPitchInput(LookAxisVector.Y);
+}
+
+void AAnimalPlayerController::HandleMove(const FInputActionValue& InputActionValue)
+{
+	// Input is a Vector2D
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+
+	// Add movement to the Player's Character Pawn.
+	if (PlayerCharacter)
+	{
+		PlayerCharacter->AddMovementInput(PlayerCharacter->GetActorForwardVector(), MovementVector.Y);
+		PlayerCharacter->AddMovementInput(PlayerCharacter->GetActorRightVector(), MovementVector.X);
+	}
+}
+
+void AAnimalPlayerController::HandleJump()
+{
+	// Input is "Digital"
+
+	// Make the Player's Character Pawn jump
+	if (PlayerCharacter)
+	{
+		PlayerCharacter->Jump();
+	}
+}
--- Crystanimals/AnimalPlayerController.h
+++ Crystanimals/AnimalPlayerController.h
@@ -4,6 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "AnimalCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
 #include "AnimalPlayerController.generated.h"
 
 /**
@@ -21,21 +24,31 @@ public:
 
 	// The Input Action to map to looking around.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Input|Character Movement")
-	UInputAction* ActionLook = nullptr;
+	TObjectPtr<UInputAction> ActionLook = nullptr;
 
 	// The Input Action to map to jumping.
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Input|Character Movement")
-	UInputAction* ActionJump = nullptr;
+	TObjectPtr<UInputAction> ActionJump = nullptr;
+
+	// The Input Mapping Context to use.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Player Input|Character Movement")
+	TObjectPtr<UInputMappingContext> InputMappingContext = nullptr;
 
 protected:
+	virtual void OnPossess(APawn* aPawn) override;
+	virtual void OnUnPossess() override;
+
+	void HandleLook(const FInputActionValue& InputActionValue);
+	void HandleMove(const FInputActionValue& InputActionValue);
+	void HandleJump();
 
 private:
 	// Used to store a reference to the InputComponent cast to an EnhancedInputComponent.
 	UPROPERTY()
-	UEnhancedInputComponent* EnhancedInputComponent = nullptr;
+	TObjectPtr<UEnhancedInputComponent> EnhancedInputComponent = nullptr;
 
 	// Used to store a reference to the controlled pawn.
 	UPROPERTY()
-	AAnimalCharacter* PlayerCharacter = nullptr;
+	TObjectPtr<AAnimalCharacter> PlayerCharacter = nullptr;
 	
 };
